d0422d38171c332e6e29dfb379a70caddacd5b57
Jorge Baptista <jorge-baptista@outlook.com>
2018-08-12 14:09:48 +0100

    Added Smooth Follow script to camera; Implemented shooting system [WIP].

--- LD-Jam/Assets/Scripts/CameraScript.cs
+++ LD-Jam/Assets/Scripts/CameraScript.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    [Header("Smooth Follow Settings")]
+    [Space]
+    [SerializeField]
+    private float speed = 0.2f;
+
+    [Header("Explosion Shake")]
+    [Space]
+    [SerializeField]
+    private float shakeDuration = 0.5f;
+    [SerializeField]
+    private float shakeRadius = 2f;
+
+
+    [Space]
     [SerializeField]
     private Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 lastOffsetPosition = Vector3.zero;
+
+    public static CameraScript instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
@@ -17,6 +48,32 @@ public class CameraScript : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), ref velocity, speed);
+    }
+
+    public void ShakeCamera()
+    {
+        StopAllCoroutines();
+        StartCoroutine(DoShake(shakeDuration, shakeRadius));
+    }
+
+    private IEnumerator DoShake(float duration, float radius)
+    {
+        while (duration > 0)
+        {
+            transform.localPosition -= lastOffsetPosition;
+
+            lastOffsetPosition = Random.insideUnitCircle * radius;
+
+            transform.localPosition += lastOffsetPosition;
+
+            if (duration < 0.5f)
+            {
+                radius *= 0.9f;
+            }
+
+            duration -= Time.deltaTime;
+            yield return null;
+        }
     }
 }
--- LD-Jam/Assets/Scripts/ZOrderScript.cs
+++ LD-Jam/Assets/Scripts/ZOrderScript.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class ZOrderScript : MonoBehaviour {
 
+	bool isDynamic = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (GetComponent<Rigidbody2D>() != null) isDynamic = true;
+		if (GetComponentInParent<Rigidbody2D>() != null) isDynamic = true;	
+		if (GetComponentInChildren<Rigidbody2D>() != null) isDynamic = true;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		Vector3 pos = transform.position;
-		BoxCollider2D bc = GetComponent<BoxCollider2D>();		
-		if (bc == null) bc = GetComponentInParent<BoxCollider2D>();
-		if (bc != null) {
-			pos.z = pos.y + bc.offset.y;
-		} else {
-			pos.z = pos.y;
+		Renderer renderer = GetComponent<Renderer>();
+		if (renderer != null) {
+			Vector3 pos = transform.position;
+			pos.z = renderer.bounds.min.y;
+			transform.position = pos;
 		}
-		transform.position = pos;
+		if (!isDynamic) Destroy(this); //remove script if object has no Rigidbody2D
+
+		// Vector3 pos = transform.position;
+		// BoxCollider2D bc = GetComponent<BoxCollider2D>();		
+		// if (bc == null) bc = GetComponentInParent<BoxCollider2D>();
+		// if (bc != null) {
+		// 	pos.z = pos.y + bc.offset.y;
+		// } else {
+		// 	pos.z = pos.y;
+		// }
+		// transform.position = pos;
 	}
 }
