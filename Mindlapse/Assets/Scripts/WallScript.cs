8d574111991e8f36b86542f6b5f66857b0325cb2
Jorge Baptista <jorge-baptista@outlook.com>
2018-08-14 02:15:31 +0100

    Changed project name, added distribution folders.

---
+++ Mindlapse/Assets/Scripts/WallScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    [SerializeField]
+    private RoomScript roomToScan;
+
+    private SpriteRenderer mySpriteRenderer;
+    private Collider2D myCollider2D;
+
+    private void Awake()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myCollider2D = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        ScanRoom();
+    }
+
+    public void ScanRoom()
+    {
+        if (!roomToScan.isBroken)
+        {
+            mySpriteRenderer.enabled = false;
+            myCollider2D.enabled = false;
+        }
+        else
+        {
+            mySpriteRenderer.enabled = true;
+            myCollider2D.enabled = true;
+        }
+    }
+}
