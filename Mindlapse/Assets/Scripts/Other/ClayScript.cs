6847a86a608e0225535e06c2f845c7a4ecb02259
Jorge Baptista <jorge-baptista@outlook.com>
2018-09-06 00:19:01 +0100

    Reorganized file structure. Added promotional Assets.

---
+++ Mindlapse/Assets/Scripts/Other/ClayScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClayScript : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 5f;
+
+    [Space]
+    [SerializeField]
+    private int clayPoints = 1;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private float initialPosY;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        initialPosY = transform.position.y;
+
+        isJumping = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJumping)
+        {
+            isJumping = false;
+
+            myRigidbody2D.velocity = Vector2.up * jumpForce;
+        }
+        else if (transform.position.y < initialPosY)
+        {
+            myRigidbody2D.isKinematic = true;
+            myRigidbody2D.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponentInParent<PlayerScript>().UpdateClayPoints(clayPoints);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Mindlapse/Assets/Scripts/Other/RandomizeAnimationStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimationStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator>().Play("", -1, Mathf.Repeat(transform.position.x, 1.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
